5d60afc6a74b9229742aa5eae4bf9b656f6d6d6f
Srinivasan Govindarasu <srinivasan.govindarasu@finzly.com>
2025-01-02 21:56:36 +0530

    initialCommit

---
+++ src/main/java/com/srini/gslv/GslvApplication.java
@@ -0,0 +1,13 @@
+package com.srini.gslv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GslvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GslvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srini/gslv/GslvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srini.gslv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GslvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
